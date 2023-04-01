f2dc8d3610f1079f877870a2614f5e38f72088d3
solorzadev <osolorzano22@gmail.com>
2023-03-31 19:23:40 -0600

    linkedLists

--- hashTable/hastable.js
+++ hashTable/hastable.js
@@ -29,6 +29,44 @@ class HashTable {
       }
       return undefined;
     }
+    delete(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        let itemDeleted
+        if (currentBucket) {
+          for (let i = 0; i < currentBucket.length; i++) {
+            if (currentBucket[i][0] === key) {
+              const index = i
+              itemDeleted = currentBucket[i]
+              if (currentBucket.length > 1) {
+                for (let i = index; i < currentBucket.length - 1; i++) {
+                  this.data[address][0] = this.data[address][i + 1];
+                }
+                delete this.data[address][currentBucket.length - 1]
+                console.log("Se ha eliminado el elemento");
+                return itemDeleted
+              } else {
+                delete this.data[address];
+                console.log("Se ha liberado el Address");
+                return itemDeleted
+              }
+            }
+          }
+        }
+        console.log("No se encontro ningun elemento a borrar");
+        return undefined
+    }
+    getAllKeys(){
+        const array = this.data
+        let key = []
+    
+        array.forEach((e) => {
+          e.forEach((k) => {
+            key.push(k[0]);
+          })
+        });
+        return key;
+      }
   }
   
   const myHashTable = new HashTable(50);
